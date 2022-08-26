9a44398f7cbc3772254d4ae102a5267c51594758
Adam <adam.gorzelak3@outlook.com>
2022-08-26 10:53:18 +0200

    complete webside

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const firstSide = document.querySelector('.container')
 const secondSide = document.querySelector('.answer')
 const btnSumbit = document.querySelector('.sumbit')
-
+const inputs = document.querySelectorAll('input')
+const spanRate = document.querySelector('.rate-info')
+let rate
 const handleActive = () => {
 	firstSide.classList.add('close')
 	secondSide.classList.remove('close')
 }
 
 btnSumbit.addEventListener('click', handleActive)
+
+inputs.forEach(input => {
+	input.addEventListener('click', e => {
+		rate = e.target.value
+		spanRate.textContent = rate
+	})
+})
